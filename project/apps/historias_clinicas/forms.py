22ccc56101c25a06358e08a0baa11e1d80a36f3c
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2024-02-17 00:27:11 -0300

    Refactor formulario para historias clinicas

--- project/apps/historias_clinicas/forms.py
+++ project/apps/historias_clinicas/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from . import models
-
+from home.models import User
 
 class HistoriaClinicaForm(forms.ModelForm):
+    paciente = forms.ModelChoiceField(queryset=User.objects.filter(groups__name='Pacientes'))
+    
     class Meta:
         model = models.HistoriaClinica
         fields = ['paciente', 'antecedentes', 'alergias', 'observaciones']
