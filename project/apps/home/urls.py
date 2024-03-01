615dc5946709a0c915a7390d036cb9c49b6ca828
Miguel Angel Rizzi <123975192+MiguelRizzi@users.noreply.github.com>
2024-02-29 22:35:28 -0300

    se eliminan comentarios en home/views.py

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -4,19 +4,12 @@ from django.views.generic import TemplateView
 from .views import HomeView
 
 urlpatterns = [
-
-    #path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
-    #path('', HomeView.as_view(), name='home'),
-
     path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
-    #path('login/', LoginView.as_view(), name='login'),
-    #path('register/', RegisterView.as_view(), name='register'),
-    #path('logout/', LogoutView.as_view(), name='logout'),
     #urls ubi provisoria
-    path('dashboard', TemplateView.as_view(template_name='home/dashboard.html'), name='dashboard'),
-    path('examenes', TemplateView.as_view(template_name='examenes/index.html'), name='examenes'),
-    path('perfiles_lipidicos', TemplateView.as_view(template_name='examenes/perfiles_lipidicos.html'), name='perfiles_lipidicos'),
-    path('perfil_lipidico', TemplateView.as_view(template_name='examenes/perfil_lipidico.html'), name='perfil_lipidico'),
-    path('hormonas_tiroideas', TemplateView.as_view(template_name='examenes/hormonas_tiroideas.html'), name='hormonas_tiroideas'),
-    path('hormonas_tiroideas_all', TemplateView.as_view(template_name='examenes/hormonas_tiroideas_all.html'), name='hormonas_tiroideas_all'),
+    path('dashboard/', TemplateView.as_view(template_name='home/dashboard.html'), name='dashboard'),
+    path('examenes/', TemplateView.as_view(template_name='examenes/index.html'), name='examenes'),
+    path('perfiles_lipidicos/', TemplateView.as_view(template_name='examenes/perfiles_lipidicos.html'), name='perfiles_lipidicos'),
+    path('perfil_lipidico/', TemplateView.as_view(template_name='examenes/perfil_lipidico.html'), name='perfil_lipidico'),
+    path('hormonas_tiroideas/', TemplateView.as_view(template_name='examenes/hormonas_tiroideas.html'), name='hormonas_tiroideas'),
+    path('hormonas_tiroideas_all/', TemplateView.as_view(template_name='examenes/hormonas_tiroideas_all.html'), name='hormonas_tiroideas_all'),
 ]
