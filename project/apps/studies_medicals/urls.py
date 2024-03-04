516b9454989add9377664e92ddcd43d27b9faaa9
rei <rei@localhost.localdomain>
2024-03-04 01:50:23 -0500

    changes frontend, views

--- project/apps/studies_medicals/urls.py
+++ project/apps/studies_medicals/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import (
+    ListTypeStudieMedical,
     ListStudiesMedicalsView,
+    ListStudiesMedicalsInTypesView,
     StudieMedicalPatientPDF,
-    DetailStudieMedicalView, 
+    DetailStudieMedicalView,
     CreateStudieMedical, 
     UpdateStudieMedical, 
     delete_studieMedical_view
@@ -11,6 +13,10 @@ from .views import (
 
 urlpatterns = [
     path('', ListStudiesMedicalsView.as_view(), name='studiesmedicals_list'),
+    path('type/', ListTypeStudieMedical.as_view(), name='studiesmedicals_type'),
+    path('type/<int:pk>', ListTypeStudieMedical.as_view(), name='studiesmedicals_type'),
+    path('type/studies/<int:pk_type>', ListStudiesMedicalsInTypesView.as_view(), name='studiesmedicals_studies'),
+    path('type/studies/<int:pk_patient>/<int:pk_type>', ListStudiesMedicalsInTypesView.as_view(), name='studiesmedicals_studies'),
     path('create', CreateStudieMedical.as_view(), name='studiesmedicals_create'),
     path('detail/<int:pk>', DetailStudieMedicalView.as_view(), name='studiesmedicals_detail'),
     path('pdf/<int:pk>', StudieMedicalPatientPDF.as_view(), name='studiesmedicals_pdf_patient'),
--- project/apps/studies_medicals/utils.py
+++ project/apps/studies_medicals/utils.py
@@ -21,3 +21,29 @@ def render_to_pdf(template_src, context_dict={}):
 def get_rol_user(user, group):
     if user.groups.filter(name=group).exists():
         return True
+    
+#Funcion para traer tipos de estudios de cierto usuario
+def get_types_studies_user(studies_medicals):
+    type_studies = []
+    seen_studies = {}
+            
+    for study in studies_medicals:
+        study.type_studie.date = study.date_joined
+        study.type_studie.patient = study.patient
+        if study.type_studie.id not in seen_studies:
+            type_studies.append(study.type_studie)
+            seen_studies[study.type_studie.id] = study.type_studie
+    
+    return type_studies
+
+#funcion para no tener usuarios de cierto estudios duplicados
+def get_users_studies_medicals(studies_medicals):
+    user_studies_medicals = []
+    seen_users_studies_medicals = {}
+    
+    for studie_medical in studies_medicals:
+        if studie_medical.patient.id not in seen_users_studies_medicals:
+            user_studies_medicals.append(studie_medical)
+            seen_users_studies_medicals[studie_medical.patient.id] = studie_medical.patient
+    
+    return user_studies_medicals
