dddb4b2af19ec6f92841ee16f5ac222a848d98a4
rei <rei@localhost.localdomain>
2024-03-06 12:28:22 -0500

    Changes settings my pc

--- project/config/settings.py
+++ project/config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ["MiguelRizzi.pythonanywhere.com"]
+ALLOWED_HOSTS = ['127.0.0.1', "MiguelRizzi.pythonanywhere.com"]
 
 
 # Application definition
@@ -84,13 +84,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mysql': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
-    }
+    },
 }
 
 
